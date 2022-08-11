097babbed008087cfaa82bcbbf5734a4dc21d7b2
Henrique Madureira <henrique.madureira@seuconsumo.com.br>
2022-08-11 16:16:30 -0300

    Automação de Testes com Selenium Web Driver e C#

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# Automa-oSeleniumCSharp
-Automação de Testes com Selenium e C#
+Selenium WebDriver em C#
+===========
+
+Este repositório contém exemplos de código do Webdriver, exercícios e tutoriais para Analistas de Testes | QA e desenvolvedores e demais profissionais de TI. :)
+
+**OBSERVAÇÃO:** atualmente é compatível com o ChromeDriver.
+
+## Site de demonstração para praticar Webdriver:
+Aqui está uma lista de sites onde você pode praticar o Selenium webdriver. Você achará a lista incrivelmente útil, pois cobrirá muitos dos seus cenários de casos de uso de automação da Web em tempo real. Alguns dos exemplos comuns incluem testes de uma página de login, formulários de registro online e automatização de reservas de voos.
+ 
+- http://newtours.demoaut.com/
+- https://ultimateqa.com/automation
+- https://automacaocombatista.herokuapp.com/
+- https://phptravels.com/demo/
+- https://www.saucedemo.com/
+
+
+## Pré-requisitos
+- Windows SO
+- Visual Studio
---
+++ Selenium/pages/HomePage.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Selenium.core.browsers;
+
+namespace Selenium.pages
+{
+    public class HomePage
+    {
+        private readonly IBrowser _browser;
+
+        public HomePage(IBrowser browser)
+        {
+            _browser = browser;
+        }
+
+        public RegisterPage Register()
+        {
+            Console.WriteLine("Navegando para a página de registro");
+            _browser.Page.GoToUrl("http://www.google.com");
+            return new RegisterPage(_browser);
+        }
+
+        public RegisterPage OpenYahooPage()
+        {
+            Console.WriteLine("Navegando para a página do Yahoo");
+            _browser.Page.GoToUrl("http://www.yahoo.com");
+            return new RegisterPage(_browser);
+        }
+
+        public RegisterPage OpenGooglePage()
+        {
+            Console.WriteLine("Navegando para a página do Google");
+            _browser.Page.GoToUrl("http://www.google.com");
+            return new RegisterPage(_browser);
+        }
+
+        public RegisterPage OpenGithubPage()
+        {
+            Console.WriteLine("Navegando para a página do Github");
+            _browser.Page.GoToUrl("http://www.github.com");
+            return new RegisterPage(_browser);
+        }
+    }
+}
